2d2242488e01b6d4e15e574b1b6608ab9df56ea9
Kasen <18170166+imkasen@users.noreply.github.com>
2022-05-06 17:20:27 +0800

    update format

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,7 @@
 -- 快捷键映射
 
 local opts = {noremap = true, silent = true}
--- local term_opts = {silent = true}
+local term_opts = {silent = true}
 
 -- 缩短函数名
 local keymap = vim.api.nvim_set_keymap
@@ -24,15 +24,18 @@ keymap("n", "<leader>q", ":q<CR>", opts)   -- 退出 Nvim
 keymap("n", "<leader>qb", ":bd<CR>", opts) -- 退出当前 buffer
 keymap("n", "<leader>w", ":w<CR>", opts)   -- 保存
 
+keymap("n", "<S-h>", "^", opts) -- 跳转到行首
+keymap("n", "<S-l>", "$", opts) -- 跳转到行尾
+
 -- 分屏
-keymap("n", "wv", "<C-w>v", opts) -- 垂直分割窗口到右侧
-keymap("n", "ws", "<C-w>s", opts) -- 横向分割窗口到下侧
+keymap("n", "<C-v>", "<C-w>v", opts) -- 垂直分割窗口到右侧
+keymap("n", "<C-s>", "<C-w>s", opts) -- 横向分割窗口到下侧
 
 -- 光标在分屏间跳转
-keymap("n", "wh", "<C-w>h", opts)
-keymap("n", "wj", "<C-w>j", opts)
-keymap("n", "wk", "<C-w>k", opts)
-keymap("n", "wl", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 -- 打开位于左侧的文件浏览
 keymap("n", "<leader>e", ":Lexplore 30<cr>", opts)
 
@@ -42,16 +45,13 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
-keymap("n", "<S-h>", "^", opts) -- 跳转到行首
-keymap("n", "<S-l>", "$", opts) -- 跳转到行尾
-
 -- Buffer 跳转
-keymap("n", "<A-l>", ":bnext<CR>", opts)
 keymap("n", "<A-h>", ":bprevious<CR>", opts)
+keymap("n", "<A-l>", ":bnext<CR>", opts)
 
 -- Insert --
--- 按 jk 返回 normal 模式
-keymap("i", "jk", "<ESC>", opts)
+-- 按 jj 返回 normal 模式
+keymap("i", "jj", "<ESC>", opts)
 
 -- Visual --
 -- ">" "<" 调整缩进
@@ -65,14 +65,14 @@ keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Visual Block --
 -- Move text up and down
--- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
--- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,7 +7,7 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-    vim.notify("Installing packer.nvim......")
+    vim.notify("Installing packer.nvim...")
     packer_bootstrap = fn.system({
         "git",
         "clone",
@@ -16,7 +16,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
         "https://github.com/wbthomason/packer.nvim",
         install_path,
     })
-    vim.notify("close and reopen NeoVim......")
+    vim.notify("close and reopen NeoVim...")
     vim.cmd [[packadd packer.nvim]]
 end
 
@@ -31,7 +31,7 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    vim.notify("packer.nvim is not installed")
+    vim.notify("packer.nvim is not installed!")
     return
 end
 
